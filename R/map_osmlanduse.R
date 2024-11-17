d269a2d73ab02a986f75fe7fcc94bf3088fa3c55
Andrés Esteban Duhour <andres@strawberry>
2024-11-17 17:02:18 -0300

    I removed function and update documentation

--- R/map_osmlanduse.R
+++ R/map_osmlanduse.R
@@ -7,15 +7,13 @@
 #' @return A tmap map
 #' @examples
 #' landuse <- get_osmlanduse()
-#' measures <- measure_osmlanduse(landuse)
 #' data(clc)
-#' classified <- classify_osmlanduse(measures,clc)
+#' classified <- classify_osmlanduse(measures,clc$class_name, clc$osm_tag)
 #' map <- map_osmlanduse(classified)
 #' map
 #' @importFrom tmap tm_shape
 #' @importFrom tmap tm_polygons
-#' @importFrom tmap tm_title
-#' @importFrom tmap tm_scalebar
+#' @importFrom tmap tm_scale_bar
 #' @importFrom tmap tm_compass
 #' @importFrom tmap tm_credits
 #' @export
@@ -23,9 +21,9 @@ map_osmlanduse <- function(classified, title = "osmlanduseR map"){
   map <-  tm_shape(classified) + tm_polygons(fill = "class_name") +
     tm_title(title) +
     tm_compass(position = c("left","top")) +
-    tm_scalebar(width = 10) +
+    tm_scale_bar(width = 10) +
     tm_credits(paste("\u00a9 OpenStreetMap contributors")) +
-    tm_layout(legend.frame = FALSE)# legend.position = c("left","top")
+    tm_layout(legend.frame = FALSE, title = title) # legend.position = c("left","top")
   map
 }
 
